3c98d35cccc9f8d2dd06900ab0976fafa2bbf425
Aniket10develops <66328410+Aniket10develops@users.noreply.github.com>
2021-10-01 15:15:43 +0530

    Create rotatearrayby1.cpp
    This cpp program rotates array by 1 element

---
+++ rotatearrayby1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void rotate(int arr[], int n)
+{
+    int temp=arr[n-1];
+    for(int i=n-1;i>0;--i){
+        arr[i]=arr[i-1];
+    }
+    arr[0]=temp;
+    for(int i=0;i<n;i++)cout<<arr[i]<<" ";
+   
+}
+int main() {
+	//code
+	int arr[]={1,2,3,4,5};
+	rotate(arr,5);
+	return 0;
+}
